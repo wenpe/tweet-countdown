dfacf88331aca243ab6ff946f485fb80505e2f4e
wkamuy <wkamuy@gmail.com>
2021-02-15 08:36:26 +0900

    add: tweet

--- src/main.ts
+++ src/main.ts
@@ -10,18 +10,35 @@ async function run(): Promise<void> {
       access_token_secret: core.getInput('access_token_secret')
     })
 
-    client.post(
-      'statuses/update',
-      {status: core.getInput('tweet_body')},
-      error => {
-        if (!error) {
-          console.log('Succeeded!')
-        } else {
-          console.log('Couldnt tweet.')
-          console.log(error)
-        }
+    const getJstDate = (dateString = '') => {
+      const jstOffset = 9 * 60
+
+      if (dateString !== '') {
+        const date = new Date(dateString)
+        date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+        return date
+      } else {
+        const date = new Date()
+        date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+        return date
       }
-    )
+    }
+
+    const now = getJstDate()
+    const targetDate = getJstDate('2021/02/28')
+    const diffTime = targetDate.getTime() - now.getTime()
+    var diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
+
+    client.post('statuses/update', {status: diffDay.toString()}, error => {
+      if (!error) {
+        console.log('Succeeded!')
+      } else {
+        console.log('Couldnt tweet.')
+        console.log(error)
+      }
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
