48253f1292ba8ea71cc72efef6a58ece7be7bb9c
wkamuy <wkamuy@gmail.com>
2021-02-15 08:13:15 +0900

    add: tweet

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,6 @@
 import * as core from '@actions/core'
 import Twitter from 'twitter'
 
-const getTweetMessage = async () => {
-  const getJstDate = (dateString = '') => {
-    const jstOffset = 9 * 60
-
-    if (dateString !== '') {
-      const date = new Date(dateString)
-      date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-      return date
-    } else {
-      const date = new Date()
-      date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-      return date
-    }
-  }
-
-  const now: Date = getJstDate()
-  const targetDate: Date = getJstDate(core.getInput('target_date'))
-
-  const diffTime = targetDate.getTime() - now.getTime()
-
-  const diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
-
-  const tweet_body = `
-    ${core.getInput('start_message')}
-
-    ${diffDay}日
-
-    ${core.getInput('end_message')}
-    `
-  return tweet_body
-}
-
 async function run(): Promise<void> {
   try {
     const client = new Twitter({
@@ -44,15 +10,17 @@ async function run(): Promise<void> {
       access_token_secret: core.getInput('access_token_secret')
     })
 
-    await getTweetMessage().then(result => {
-      client.post('statuses/update', {status: result}, error => {
+    client.post(
+      'statuses/update',
+      {status: core.getInput('tweet_body')},
+      error => {
         if (!error) {
           console.log('Succeeded!')
         } else {
           console.log('Couldnt tweet.')
         }
-      })
-    })
+      }
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
