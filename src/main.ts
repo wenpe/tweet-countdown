945b5cfae0263843f194ddb8c3d3859140c984f4
wkamuy <wkamuy@gmail.com>
2021-02-15 08:09:58 +0900

    add: tweet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,40 @@
 import * as core from '@actions/core'
 import Twitter from 'twitter'
 
+const getTweetMessage = async () => {
+  const getJstDate = (dateString = '') => {
+    const jstOffset = 9 * 60
+
+    if (dateString !== '') {
+      const date = new Date(dateString)
+      date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+      return date
+    } else {
+      const date = new Date()
+      date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+      return date
+    }
+  }
+
+  const now: Date = getJstDate()
+  const targetDate: Date = getJstDate(core.getInput('target_date'))
+
+  const diffTime = targetDate.getTime() - now.getTime()
+
+  const diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
+
+  const tweet_body = `
+    ${core.getInput('start_message')}
+
+    ${diffDay}日
+
+    ${core.getInput('end_message')}
+    `
+  return tweet_body
+}
+
 async function run(): Promise<void> {
   try {
     const client = new Twitter({
@@ -10,45 +44,14 @@ async function run(): Promise<void> {
       access_token_secret: core.getInput('access_token_secret')
     })
 
-    const getJstDate = (dateString = '') => {
-      const jstOffset = 9 * 60
-
-      if (dateString !== '') {
-        const date = new Date(dateString)
-        date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-        return date
-      } else {
-        const date = new Date()
-        date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-        return date
-      }
-    }
-
-    const now: Date = getJstDate()
-    const targetDate: Date = getJstDate(core.getInput('target_date'))
-
-    const diffTime = targetDate.getTime() - now.getTime()
-
-    const diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
-
-    const tweet_body = `
-    ${core.getInput('start_message')}
-
-    ${diffDay}日
-
-    ${core.getInput('end_message')}
-    `
-
-    console.log(tweet_body)
-
-    client.post('statuses/update', {status: tweet_body}, error => {
-      if (!error) {
-        console.log('Succeeded!')
-      } else {
-        console.log('Couldnt tweet.')
-      }
+    await getTweetMessage().then(result => {
+      client.post('statuses/update', {status: result}, error => {
+        if (!error) {
+          console.log('Succeeded!')
+        } else {
+          console.log('Couldnt tweet.')
+        }
+      })
     })
   } catch (error) {
     core.setFailed(error.message)
