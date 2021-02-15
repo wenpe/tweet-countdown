18cf9af3f0efb89a315df76a318eb2acd5bdc5c8
wkamuy <wkamuy@gmail.com>
2021-02-15 11:34:55 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import Twitter from 'twitter'
 
-async function run(): Promise<void> {
+async function run(message: string): Promise<void> {
   try {
     const client = new Twitter({
       consumer_key: core.getInput('consumer_key'),
@@ -10,33 +10,11 @@ async function run(): Promise<void> {
       access_token_secret: core.getInput('access_token_secret')
     })
 
-    const getJstDate = (dateString = '') => {
-      const jstOffset = 9 * 60
-
-      if (dateString !== '') {
-        const date = new Date(dateString)
-        date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-        return date
-      } else {
-        const date = new Date()
-        date.setTime(date.getTime() + jstOffset * 60 * 1000)
-
-        return date
-      }
-    }
-
-    const now = getJstDate()
-    const targetDate = getJstDate('2021/02/28')
-    const diffTime = targetDate.getTime() - now.getTime()
-    var diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
-
-    client.post('statuses/update', {status: diffDay.toString()}, error => {
+    client.post('statuses/update', {status: message}, error => {
       if (!error) {
         console.log('Succeeded!')
       } else {
         console.log('Couldnt tweet.')
-        console.log(error)
       }
     })
   } catch (error) {
@@ -44,4 +22,25 @@ async function run(): Promise<void> {
   }
 }
 
-run()
+const getJstDate = (dateString = '') => {
+  const jstOffset = 9 * 60
+
+  if (dateString !== '') {
+    const date = new Date(dateString)
+    date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+    return date
+  } else {
+    const date = new Date()
+    date.setTime(date.getTime() + jstOffset * 60 * 1000)
+
+    return date
+  }
+}
+
+const now = getJstDate()
+const targetDate = getJstDate('2021/02/28')
+const diffTime = targetDate.getTime() - now.getTime()
+var diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24) + 1)
+
+run(diffDay.toString())
